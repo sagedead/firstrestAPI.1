b0ab74ab7c5da8a03cd6b91b61009a55af2e4fa7
sagedead <lydiagebre7@gmail.com>
2025-05-22 18:51:21 +0200

    updated

--- src/main/java/pl/edu/vistula/RestAPI/Product/API/domain/Product.java
+++ src/main/java/pl/edu/vistula/RestAPI/Product/API/domain/Product.java
@@ -6,22 +6,35 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "Products")
-
 public class Product {
 
     @Id
-    @GeneratedValue
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
 
-    public Product(String name) { this.name = name;  }
 
-    public Long getId() { return id; }
+    public Product() {}
 
-    public String getName() { return name; }
 
-    public void setId(long id) { this.id = id; }
+    public Product(String name) {
+        this.name = name;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
